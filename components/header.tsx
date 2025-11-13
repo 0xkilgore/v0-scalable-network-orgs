4765fe8ec6f84010b452ddbcdb1e8ced0705edb5
0xkilgore <0xkilgore@gmail.com>
2025-11-13 01:57:17 -0600

    Add SNO logo to top right corner
    - Updated header component to display SNO logo opposite Powerhouse logo
    - Added sno-logo.png to public directory
    - Fixed header positioning with both logos

--- components/header.tsx
+++ components/header.tsx
@@ -3,23 +3,38 @@ import Link from "next/link"
 
 export function Header() {
   return (
-    <header className="fixed top-0 left-0 z-50 p-6 sm:p-8">
-      <Link
-        href="https://www.powerhouse.inc"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="block transition-opacity hover:opacity-80"
-      >
+    <>
+      {/* Powerhouse logo - top left */}
+      <header className="fixed top-0 left-0 z-50 p-6 sm:p-8">
+        <Link
+          href="https://www.powerhouse.inc"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block transition-opacity hover:opacity-80"
+        >
+          <Image
+            src="/powerhouse-logo.svg"
+            alt="Powerhouse"
+            width={180}
+            height={21}
+            className="h-5 w-auto sm:h-6"
+            priority
+          />
+        </Link>
+      </header>
+
+      {/* SNO logo - top right */}
+      <div className="fixed top-0 right-0 z-50 p-6 sm:p-8">
         <Image
-          src="/powerhouse-logo.svg"
-          alt="Powerhouse"
-          width={180}
-          height={21}
-          className="h-5 w-auto sm:h-6"
+          src="/sno-logo.png"
+          alt="SNO"
+          width={80}
+          height={32}
+          className="h-8 w-auto"
           priority
         />
-      </Link>
-    </header>
+      </div>
+    </>
   )
 }
 
